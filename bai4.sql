3e558d7acd79393c789edfc59be33a9f9783424a
Vuduc2907 <vduc9262@gmail.com>
2026-04-15 09:43:28 +0700

    Add files via upload

---
+++ bai4.sql
@@ -0,0 +1,16 @@
+create database bai4 ;
+
+use bai4;
+
+CREATE TABLE USERS (
+    UserID INT ,
+    FullName VARCHAR(100) NOT NULL,
+    Phone VARCHAR(15) NOT NULL,
+    Email VARCHAR(100) UNIQUE,
+    PasswordHash VARCHAR(255) NOT NULL,
+    Status VARCHAR(20) DEFAULT 'Active',
+    CHECK (Status IN ('Active', 'Inactive', 'Locked', 'Suspended')),
+    CreatedAt DATETIME ,
+    UpdatedAt DATETIME ,
+    PRIMARY KEY (UserID)
+);
